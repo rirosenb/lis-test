3589cfde7b7d5a6b85608881b5d5cf7117df97de
James V. Geiger <james.v.geiger@nasa.gov>
2018-09-20 13:38:47 -0400

    Add CLM7

--- plugins/LIS_lsm_pluginMod.F90
+++ plugins/LIS_lsm_pluginMod.F90
@@ -210,6 +210,10 @@ subroutine LIS_lsm_plugin
    use noah4_lsmMod,  only : noah4_lsm_ini
 #endif
 
+#if ( defined SM_CLM7 )
+   use clm7_lsmMod,  only : clm7_lsm_ini
+#endif
+
 #if ( defined SM_LSM_TEMPLATE )
    external template_main
    external template_setup
